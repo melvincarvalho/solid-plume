c909b50e43e5da7cebdcf91ed30e90549eee55ce
Melvin Carvalho <melvincarvalho@gmail.com>
2015-12-08 22:36:02 +0100

    Config

--- app/config.js
+++ app/config.js
@@ -3,11 +3,11 @@ var Plume = Plume || {};
 
 // Init some default config values
 Plume.config = {
-    owner: '',
-    title: "My Plume blog",
-    tagline: "Light as a feather",
+    owner: 'http://melvincarvalho.com/#me',
+    title: "Melvster's blog",
+    tagline: "riverrun!",
     picture: "img/logo.svg",
     fadeText: true,
     defaultPath: 'posts',
-    dataContainer: ''
+    dataContainer: 'https://melvin.databox.me/Public/blog/'
 };
